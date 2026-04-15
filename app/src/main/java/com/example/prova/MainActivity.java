37be70444b44623da07491b2cdbb851c67d901ce
Cohen <arielecohendepaula@gmail.com>
2026-04-15 19:52:05 -0300

    feat: adicionando classe produto no main

--- app/src/main/java/com/example/prova/MainActivity.java
+++ app/src/main/java/com/example/prova/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.prova;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNome, editTextCodigo, getEditTextPreco, editTextQtdEstoque;
+    private ProdutoDao produtoDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextCodigo = findViewById(R.id.editTextCodigo);
+        getEditTextPreco = findViewById(R.id.editTextPreco);
+        editTextQtdEstoque = findViewById(R.id.editTextQtdEstoque);
+
+        Button buttonSave = findViewById(R.id.buttonSave);
+        Button buttonReport = findViewById(R.id.buttonReport);
+        Button buttonUpdate = findViewById(R.id.buttonUpdate);
+        Button buttonDelete = findViewById(R.id.buttonDelete);
+
+        ProdutoDataBase db = Room.databaseBuilder(getApplicationContext(),
+                        ProdutoDataBase.class, "user-database")
+                .allowMainThreadQueries()
+                .build();
+
+        produtoDao = db.produtoDao();
     }
 }
---
+++ app/src/main/java/com/example/prova/ReportActivity.java
@@ -0,0 +1,10 @@
+package com.example.prova;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
