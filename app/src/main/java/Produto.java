8b2b7c1a216cf909cb0ef66f8e5084bbd449e927
Cohen <arielecohendepaula@gmail.com>
2026-04-15 19:24:08 -0300

    feat: Adicionando gets e sets

--- app/src/main/java/Produto.java
+++ app/src/main/java/Produto.java
@@ -12,4 +12,34 @@ public class Produto {
     private int preco;
     private int qtdEstoque;
 
+    public Produto(String nome, int codigo, int preco, int qtdEstoque) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getNome() {return nome;}
+
+    public int getCodigo() {return codigo;}
+
+    public int getPreco() {return preco;}
+
+    public int getQtdEstoque() {return qtdEstoque;}
+
+
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public void  setCodigo(int nome) {this.codigo = codigo;}
+
+    public void setPreco(int preco) {this.preco = preco;}
+
+    public void setQtdEstoque(int qtdEstoque) {this.qtdEstoque = qtdEstoque;}
+
+
 }
